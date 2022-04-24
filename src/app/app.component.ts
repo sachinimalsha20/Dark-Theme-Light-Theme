550936176091d593ba05a714cf09165850630b86
sachinimalsha20 <bh.sachini20@gmail.com>
2022-04-24 11:29:52 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test2';
+  storedTheme: string | null=localStorage.getItem('theme-color');
+  constructor() {}
+  ngOnInit() {
+  }
+  setTheme(){
+    if(this.storedTheme==='theme-dark'){
+      //toggle and update local storage
+      localStorage.setItem('theme-color','theme-light');
+      this.storedTheme=localStorage.getItem('theme-color');
+
+    }else{
+      //toggle and update local storage
+      localStorage.setItem('theme-color','theme-dark');
+      this.storedTheme=localStorage.getItem('theme-color');
+    }
+  }
+
 }
